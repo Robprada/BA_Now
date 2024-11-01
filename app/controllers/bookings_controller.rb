d26efb5b4fb08b9c87532b3f8f6ac34580927334
Valentin Cadel <104843541+valencadel@users.noreply.github.com>
2024-10-31 21:58:35 -0300

    booking controller halfway

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,31 @@
 class BookingsController < ApplicationController
-  def index
-  end
+  # def index
+  #   @bookings = Booking.all
+  # end
 
   def new
+    @experience = Experience.find(params[:id])
+    @booking = @experience.bookings.new
   end
 
   def create
+    @experince = Experience.find(params[:id])
+    @booking = @experience.bookings.new(booking_params)
+    if @booking.save
+      redirect_to root_path, notice: 'Booking created succesfully!'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_date, :hour, :tickets)
   end
 end

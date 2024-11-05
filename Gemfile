5ce89c7c1bf41cae0a1e0ad5720ca6a6ef228587
gonzalesomar <gonzales.omar@pucp.edu.pe>
2024-11-05 18:19:12 -0500

    search bar

--- Gemfile
+++ Gemfile
@@ -69,3 +69,4 @@ gem "devise"
 
 gem "cloudinary"
 gem "geocoder"
+gem "pg_search"
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -8,4 +8,10 @@ class Experience < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+                  against: %i[title description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end

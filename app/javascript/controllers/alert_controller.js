dc89e954010cb051b6e65f47320f8d7a44a41580
gonzalesomar <gonzales.omar@pucp.edu.pe>
2024-11-07 19:19:15 -0500

    sweet-alert

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,29 +1,68 @@
-import { Controller } from "@hotwired/stimulus";
-import Swal from "sweetalert2";
+// import { Controller } from "@hotwired/stimulus";
+// import Swal from "sweetalert2";
+
+// export default class extends Controller {
+//   static values = {
+//     title: String,
+//     text: String,
+//     confirmButtonText: String,
+//     cancelButtonText: String,
+//   };
+
+//   confirm(event) {
+//     event.preventDefault(); // Prevenir el envío del formulario
+
+//     Swal.fire({
+//       title: this.titleValue,
+//       text: this.textValue,
+//       icon: "warning",
+//       showCancelButton: true,
+//       confirmButtonText: this.confirmButtonTextValue,
+//       cancelButtonText: this.cancelButtonTextValue,
+//     }).then((result) => {
+//       if (result.isConfirmed) {
+//         // Si el usuario confirma, realiza el envío del formulario
+//         event.target.click(); // Simula un clic en el enlace original
+//       }
+//     });
+//   }
+// }
+
+// app/javascript/controllers/alert_controller.js
+
+import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2"
 
 export default class extends Controller {
   static values = {
+    icon: String,
     title: String,
-    text: String,
+    html: String,
     confirmButtonText: String,
-    cancelButtonText: String,
-  };
-
-  confirm(event) {
-    event.preventDefault(); // Prevenir el envío del formulario
-
+    showCancelButton: Boolean,
+    cancelButtonText: String
+  }
+  initSweetalert(event) {
+    // console.log(this.iconValue)
+    // console.log(this.titleValue)
+    // console.log(this.htmlValue)
+    // console.log(this.confirmButtonTextValue)
+    // console.log(this.showCancelButtonValue)
+    // console.log(this.cancelButtonTextValue)
+    event.preventDefault(); // Prevent the form to be submited after the submit button has been clicked
     Swal.fire({
+      icon: this.iconValue,
       title: this.titleValue,
-      text: this.textValue,
-      icon: "warning",
-      showCancelButton: true,
+      html: this.htmlValue,
       confirmButtonText: this.confirmButtonTextValue,
+      showCancelButton: this.showCancelButtonValue,
       cancelButtonText: this.cancelButtonTextValue,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Si el usuario confirma, realiza el envío del formulario
-        event.target.click(); // Simula un clic en el enlace original
+      reverseButtons: true
+    }).then((action) => {
+      if (action.isConfirmed) {
+        event.target[event.type](); // "submit"
       }
-    });
+    })
+    .catch(event.preventDefault())
   }
 }

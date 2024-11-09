1658b9352e41ee91ad11ea3aebbcd3f77e15387f
roberto prada <ing.robertoprada@gmail.com>
2024-11-09 12:05:40 -0300

    updated pagination

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,53 @@
+# class PagesController < ApplicationController
+#   skip_before_action :authenticate_user!, only: :home
+
+#   def home
+#     # @experiences = Experience.all
+#     # sql_subquery = <<~SQL
+#     #   experiences.title @@ :query
+#     #   OR experiences.description @@ :query
+#     # SQL
+#     # return unless params[:query].present?
+
+#     # @experiences = Experience.where(sql_subquery, query: "%#{params[:query]}%")
+#     # @experiences = Experience.search_by_title_and_description(params[:query])
+
+#     if params[:query].present?
+#       @experiences = Experience.search_by_title_and_description(params[:query])
+#     else
+#       @experiences = Experience.all
+#     end
+
+#     respond_to do |format|
+#       format.html # Render the default HTML view
+#       format.json do
+#         render json: { results: render_to_string(partial: "experiences", formats: [:html],
+#                                                  locals: { experiences: @experiences }) }
+#       end
+#     end
+#   end
+# end
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    # @experiences = Experience.all
-    # sql_subquery = <<~SQL
-    #   experiences.title @@ :query
-    #   OR experiences.description @@ :query
-    # SQL
-    # return unless params[:query].present?
-
-    # @experiences = Experience.where(sql_subquery, query: "%#{params[:query]}%")
-    # @experiences = Experience.search_by_title_and_description(params[:query])
-
     if params[:query].present?
-      @experiences = Experience.search_by_title_and_description(params[:query])
+      @experiences = Experience.search_by_title_and_description(params[:query]).page(params[:page]).per(8)
     else
-      @experiences = Experience.all
+      @experiences = Experience.page(params[:page]).per(8)
     end
 
     respond_to do |format|
-      format.html # Render the default HTML view
+      format.html
       format.json do
-        render json: { results: render_to_string(partial: "experiences", formats: [:html],
-                                                 locals: { experiences: @experiences }) }
+        render json: {
+          results: render_to_string(
+            partial: "experiences",
+            formats: [:html],
+            locals: { experiences: @experiences }
+          )
+        }
       end
     end
   end
